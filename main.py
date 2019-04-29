a91fb8b8c58b9fb4f92ee038de48272726f5a1e0
鸭王 <27958875@qq.com>
2019-04-29 21:39:22 +0800

    写代码五分钟 修bug两小时
    写代码五分钟 修bug两小时

--- main.py
+++ main.py
@@ -97,7 +97,6 @@ def getConfig():
         print(time.strftime('[%H:%M:%S]') + "配置解析成功...")
     except:
         print(time.strftime('[%H:%M:%S]') + "配置解析失败 请检查格式是否正确...")
-
     try:
         global db
         db = pymysql.connect(host,user,password,db_name,port=port,charset=charset)
@@ -114,10 +113,10 @@ if __name__ == '__main__':
     print(time.strftime('[%H:%M:%S]') + "当前数据库版本为: %s " % data)
     time.sleep(1)
     data = execSQL(db, "show variables like '%general_log%';")[1]
-    print(time.strftime('[%H:%M:%S]日志状态为:') + data)
+    print(time.strftime('[%H:%M:%S]') + '日志状态为:' + data)
     if data == "OFF":
         try:
-            print(time.strftime('[%H:%M:%S]正在尝试开启日志模式...') )
+            print(time.strftime('[%H:%M:%S]') + '正在尝试开启日志模式...')
             time.sleep(1)
             try:
                 logPath = os.getcwd()
@@ -132,17 +131,16 @@ if __name__ == '__main__':
             data = execSQL(db, "set global general_log=on;")
             data = execSQL(db, "show variables like '%general_log%';")[1]
             if data == "ON":
-                print(time.strftime('[%H:%M:%S]日志模式已开启...'))
-                print(time.strftime('[%H:%M:%S]日志监听中...'))
+                print(time.strftime('[%H:%M:%S]') + '日志模式已开启...')
+                print(time.strftime('[%H:%M:%S]') + '日志监听中...')
                 log = str(execSQL(db, "show variables like 'general_log_file';")[-1])
                 logMonitor(log)
         except:
-            print(time.strftime('[%H:%M:%S]日志模式开启失败...'))
-            print(time.strftime('[%H:%M:%S]未知错误 请联系https://github.com/TheKingOfDuck/MySQLMonitor/issues反馈问题...:'))
+            print(time.strftime('[%H:%M:%S]') + '日志模式开启失败...')
+            print(time.strftime('[%H:%M:%S]') + '未知错误 请联系https://github.com/TheKingOfDuck/MySQLMonitor/issues反馈问题...:')
             exit()
     else:
-
-        print(time.strftime('[%H:%M:%S]日志监听中...'))
+        print(time.strftime('[%H:%M:%S]') + '日志监听中...')
     log = str(execSQL(db, "show variables like 'general_log_file';")[-1])
     db.close()
     logMonitor(log)
